718d13ef479d0cd628a3f1d5b46fb4e39038a64c
Rafael de Jesus Carvalho <141766102+rafaeldjcarvalho@users.noreply.github.com>
2025-02-02 18:45:24 -0300

    feat: add mysql driver

--- src/main/java/com/rafael/atendimento/AtendimentoApplication.java
+++ src/main/java/com/rafael/atendimento/AtendimentoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.rafael.atendimento.entity.User;
@@ -19,6 +20,7 @@ public class AtendimentoApplication {
 	}
 	
 	@Bean
+	@Profile("test")
 	CommandLineRunner initDatabase(UserRepository userRepository) {
 		return args -> {
 			userRepository.deleteAll();
